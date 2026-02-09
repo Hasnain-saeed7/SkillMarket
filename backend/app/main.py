1f2de26eb33f2713097bf324072ef6efd61412df
Hasnain Saeed <fa23-bai-018@isbstudent.comsats.edu.pk>
2026-02-09 10:11:37 +0500

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -27,10 +27,11 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://skill-market-theta.vercel.app",
+    allow_origins=[
+        "https://skill-market-theta.vercel.app",
         "https://skill-market-1kyjhhre4-hasnain-saeed7s-projects.vercel.app",
         "http://localhost:5173",
-    "],
+    ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allow_headers=["*"],
